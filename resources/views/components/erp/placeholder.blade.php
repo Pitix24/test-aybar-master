3b51a5d8018a226a5362a22cd793db093f87362b
Mersmith <mersmith14@gmail.com>
2026-02-06 13:00:31 -0500

    avance spiner

--- resources/views/components/erp/placeholder.blade.php
+++ resources/views/components/erp/placeholder.blade.php
@@ -1,6 +1,3 @@
-<div class="g_panel" {{ $attributes }}>
-    <div class="g_vacio">
-        <i class="fa-solid fa-spinner fa-spin fa-2x"></i>
-        <p>Cargando ...</p>
-    </div>
+<div class="g_placeholder_panel" {{ $attributes }}>
+    <div class="g_placeholder_spinner"></div>
 </div>
