dbe5c13647b8ba3d9137a8146da56e47f7416f04
Mersmith <mersmith14@gmail.com>
2026-02-06 23:58:26 -0500

    avance

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,45 +1,29 @@
-@props(['name' => null, 'open' => false, 'maxWidth' => '600px'])
-
-<div x-data="{ 
-        show: @js($open),
-        init() {
-            if (this.show) {
-                document.body.style.overflow = 'hidden';
-            }
-            $watch('show', value => {
-                if (value) {
-                    document.body.style.overflow = 'hidden';
-                } else {
-                    document.body.style.overflow = null;
-                }
-            });
-        }
-    }" x-show="show" x-on:close.stop="show = false" x-on:keydown.escape.window="show = false" x-cloak id="{{ $name }}"
-    class="g_modal_overlay">
-    <div class="g_modal_container" style="max-width: {{ $maxWidth }};" x-show="show"
-        x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0 scale-95"
-        x-transition:enter-end="opacity-100 scale-100" x-transition:leave="ease-in duration-200"
-        x-transition:leave-start="opacity-100 scale-100" x-transition:leave-end="opacity-0 scale-95"
-        @click.outside="show = false">
-        @if (isset($titulo))
-            <div class="g_modal_header">
-                <div class="g_modal_title">
-                    {{ $titulo }}
-                </div>
-                <button type="button" @click="show = false" class="g_modal_close">
-                    <i class="fa-solid fa-xmark"></i>
-                </button>
-            </div>
-        @endif
-
-        <div class="g_modal_body">
-            {{ $cuerpo ?? $slot }}
+@props([
+    'open' => false,
+    'maxWidth' => null,
+])
+<div x-data="{ open: @js($open) }" x-show="open" x-cloak @keydown.escape.window="open = false" class="g_modal">
+    <div class="modal_contenedor" @click.stop @if ($maxWidth) style="max-width: {{ $maxWidth }};" @endif>
+        <div class="modal_cerrar">
+            <button type="button" @click="open = false">
+                <i class="fa-solid fa-xmark"></i>
+            </button>
         </div>
 
-        @if (isset($pie))
-            <div class="g_modal_footer">
-                {{ $pie }}
-            </div>
-        @endif
+        <div class="modal_titulo">
+            {{ $titulo ?? '' }}
+        </div>
+
+        <div class="modal_cuerpo">
+            {{ $cuerpo ?? '' }}
+        </div>
+
+        <br>
+
+        <div class="formulario_botones g_centrar_elemento">
+            <button type="button" @click="open = false" class="guardar">
+                ACEPTAR
+            </button>
+        </div>
     </div>
 </div>
--- resources/views/modules/cliente/modal/bienvenida-cliente.blade.php
+++ resources/views/modules/cliente/modal/bienvenida-cliente.blade.php
@@ -1,13 +1,19 @@
-<div class="g_centrar_elemento">
-    <div class="g_gap_pagina" style="text-align: center;">
-        <i class="fa-solid fa-circle-check" style="font-size: 3rem; color: #36c76c; margin-bottom: 15px;"></i>
-        <p class="g_inferior">Nos alegra tenerte de vuelta. Desde aquí podrás gestionar tus proyectos, ver el estado de
-            tus solicitudes y descargar tus documentos importantes.</p>
-
-        <div class="g_margin_top_20">
-            <button type="button" class="g_boton g_boton_primary" @click="show = false">
-                Comenzar
-            </button>
-        </div>
-    </div>
+<div>
+    <p class="r_parrafo minimo justify color_1">
+        Queremos brindarte información clara y una atención más oportuna.
+    </p>
+    <p class="r_parrafo minimo justify color_1">
+        Por ello, es importante que mantengas tus datos personales actualizados.
+    </p>
+    <br>
+    <p><span class="r_negrita">Importante</span></p>
+    <br>
+    <p class="r_parrafo minimo justify color_1">Estamos optimizando nuestros procesos para ofrecerte una comunicación
+        más ágil y transparente. Te pedimos revisar
+        que la información mostrada sea correcta.
+        Si detectas alguna inconsistencia, podrás adjuntar las evidencias correspondientes o comunicarte con nuestro
+        equipo de Atención al Cliente (ATC), que estará disponible para apoyarte.
+    </p>
+    <br>
+    <p class="r_parrafo minimo justify color_1">Gracias por ayudarnos a mejorar tu experiencia.</p>
 </div>
