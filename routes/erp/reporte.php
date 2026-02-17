b709df867d48047bce0c1051095720e1b8480edb
Mersmith <mersmith14@gmail.com>
2026-02-17 10:06:56 -0500

    avance

--- routes/erp/reporte.php
+++ routes/erp/reporte.php
@@ -5,10 +5,15 @@ use App\Livewire\Erp\Reporte\Sistema\ReporteSolicitudEvidenciaPago;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['permission:modulo-reporte.ver']], function () {
-    Route::group(['middleware' => ['permission:reporte-sistema.navegacion']], function () {
+    Route::group(['middleware' => ['permission:reporte-usuario.navegacion']], function () {
         Route::prefix('reporte')->name('reporte.vista.')->group(function () {
-            Route::get('/cliente', ReporteCliente::class)->middleware('permission:reporte.cliente.ver')->name('cliente');
-            Route::get('/solicitud-evidencia-pago', ReporteSolicitudEvidenciaPago::class)->middleware('permission:reporte.solicitud-evidencia-pago.ver')->name('solicitud-evidencia-pago');
+            Route::get('/cliente', ReporteCliente::class)->middleware('permission:reporte-usuario.cliente.ver')->name('cliente');
+        });
+    });
+
+    Route::group(['middleware' => ['permission:reporte-backoffice.navegacion']], function () {
+        Route::prefix('reporte')->name('reporte.vista.')->group(function () {
+            Route::get('/solicitud-evidencia-pago', ReporteSolicitudEvidenciaPago::class)->middleware('permission:reporte-backoffice.solicitud-evidencia-pago.ver')->name('solicitud-evidencia-pago');
         });
     });
 });
