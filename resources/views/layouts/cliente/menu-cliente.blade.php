dff16515ab98d19447c1ea6437e3ee40460731b4
Mersmith <mersmith14@gmail.com>
2026-02-18 11:32:50 -0500

    avance

--- resources/views/layouts/cliente/menu-cliente.blade.php
+++ resources/views/layouts/cliente/menu-cliente.blade.php
@@ -1,5 +1,5 @@
 <div class="cliente_menu_pricipal">
-    <a href="{{ route('cliente.home') }}">
+    <a href="{{ route('cliente.home') }}" class="{{ request()->routeIs('cliente.home') ? 'active' : '' }}">
         <span>
             <i class="fa-solid fa-address-card"></i>
             Perfil
@@ -10,12 +10,12 @@
         <i class="fa-solid fa-chevron-right"></i>
     </a>
 
-    <a href="{{ route('cliente.lote') }}" class="">
+    <a href="{{ route('cliente.lote') }}" class="{{ request()->routeIs('cliente.lote') ? 'active' : '' }}">
         <span><i class="fa-solid fa-border-all"></i> Mis Proyectos</span>
         <i class="fa-solid fa-chevron-right"></i>
     </a>
 
-    <a href="{{ route('cliente.tutorial') }}" class="">
+    <a href="{{ route('cliente.tutorial') }}" class="{{ request()->routeIs('cliente.tutorial') ? 'active' : '' }}">
         <span><i class="fa-solid fa-circle-play"></i> Tutoriales</span>
         <i class="fa-solid fa-chevron-right"></i>
     </a>
@@ -23,7 +23,7 @@
     <form method="POST" action="{{ route('logout') }}">
         @csrf
         <button type="submit">
-            <span><i class="fa-solid fa-power-off"></i> Cerrar</span>
+            <span><i class="fa-solid fa-power-off"></i> Cerrar sesión</span>
         </button>
     </form>
 </div>
