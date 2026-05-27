1934384ba0f7ddc0ff414eeacdc8fe34269235cf
Pitix24 <mglp2404@gmail.com>
2026-05-27 13:15:45 -0500

    update RouteLega: Se crea rutas legales y se consolidan las rutas de LibroReclaamcion y Notariales

--- routes/erp/legal.php
+++ routes/erp/legal.php
@@ -4,6 +4,10 @@ use App\Livewire\Erp\Legal\LibroReclamacion\LibroReclamacionCrear;
 use App\Livewire\Erp\Legal\LibroReclamacion\LibroReclamacionEditar;
 use App\Livewire\Erp\Legal\LibroReclamacion\LibroReclamacionLista;
 use App\Livewire\Erp\Legal\LibroReclamacion\LibroReclamacionVer;
+use App\Livewire\Erp\Legal\TicketNotarial\TicketNotarialLista;
+use App\Livewire\Erp\Atc\Ticket\TicketCrear;
+use App\Livewire\Erp\Atc\Ticket\TicketEditar;
+use App\Livewire\Erp\Atc\Ticket\TicketVer;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['permission:modulo-legal.ver']], function () {
@@ -19,6 +23,17 @@ Route::group(['middleware' => ['permission:modulo-legal.ver']], function () {
                 }
 
                 Route::get('/editar/{id}', LibroReclamacionEditar::class)->middleware('permission:libro-reclamacion.editar')->name('editar');
+            });
+    });
+
+    Route::group(['middleware' => ['permission:ticket-notarial.navegacion']], function () {
+        Route::prefix('ticket-notarial')
+            ->name('ticket-notarial.vista.')
+            ->group(function () {
+                Route::get('/', TicketNotarialLista::class)->middleware('permission:ticket-notarial.lista')->name('todo');
+                Route::get('/crear', TicketCrear::class)->middleware('permission:ticket-notarial.crear')->name('crear');
+                Route::get('/ver/{id}', TicketVer::class)->middleware('permission:ticket-notarial.ver')->name('ver');
+                Route::get('/editar/{id}', TicketEditar::class)->middleware('permission:ticket-notarial.editar')->name('editar');
             });
     });
 });
